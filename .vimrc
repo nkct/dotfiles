4d117bf63a19e1f291cf21163af31f2e82248f63
Miłosz Żegota <miloszzegota@gmail.com>
2026-03-23 15:25:58 +0100

    implemented trivial personal package manager

--- .vimrc
+++ .vimrc
@@ -4,6 +4,7 @@
 :set autoindent
 :set wrap
 :set linebreak
+:set number
 
 :set spellfile=~/.vim/spell/en.utf-8.add
 
@@ -11,4 +12,4 @@ nnoremap <C-p> i```<Esc>o<Esc>o```<Esc>ki
 nnoremap z <C-v>
 nnoremap <C-f> k/^\d\+\.<CR>gn<C-a>/^\d\+\.<CR>
 nnoremap <C-b> j/^\d\+\.<CR>NNgn<C-x>/^\d\+\.<CR>NN
-
+nnoremap <C-q> ciw"<C-r>""<Esc>
---
+++ install/tppm.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# Entrypoint for the Trivial Personal Package Manager
+# Any file in this directory, which ends with '.pkg.sh' is taken to be an installation script
+# Installation scripts must expose two functions: info and install, as well as a _DEPS array variable
+
+set -euo pipefail
+
+USAGE="tppm --help\n     list\n     info <PKG>\n     install <PKG>\n"
+
+--help() {
+  echo -e "$USAGE"
+}
+
+list() {
+  echo "Avaliable packages:"
+  for f in *.pkg.sh; do
+    [ -e "$f" ] || continue
+    echo " - ${f%.pkg.sh}"
+  done
+}
+
+info() {
+  pkg=$1
+  if [ $# -eq 0 ]; then
+    echo -e "$USAGE"
+    return 0
+  fi
+
+  source "./$pkg.pkg.sh"
+  info
+  echo "Requires: ${_DEPS[@]}"
+}
+
+install() {
+  pkg = $1
+  if [ $# -eq 0 ]; then
+    echo -e "$USAGE"
+    return 0
+  fi
+
+  source "./$pkg.pkg.sh"
+  for dep in "${_DEPS[@]}"; do
+    if ! command -v "$dep" >/dev/null 2>&1; then
+      printf 'Error: required dependency "%s" not found.\n' "$dep" >&2
+      exit 1
+    fi
+  done
+
+  install
+}
+
+cmd="${1:-}"
+if declare -F -- "$cmd" >/dev/null; then
+  shift
+  "$cmd" "$@"
+else
+  echo "Command '$cmd' not found" >&2
+  echo -e "$USAGE"
+  exit 2
+fi
+
